0d3b058e5db0ed84e5cb9d034f5513910a9dfe72
Tojo Rabemananjara <trabema1@terpmail.umd.edu>
2021-11-09 10:30:21 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Profile } from './Profile';
+import { Directory } from './Directory';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUsername: null,
+    };
+    this.handleChoose = this.handleChoose.bind(this);
+    this.handleReturnToDirectoryClick = this.handleReturnToDirectoryClick.bind(
+      this
+    );
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  handleChoose(newUsername) {
+    this.setState({ currentUsername: newUsername });
+  }
+
+  handleReturnToDirectoryClick() {
+    this.setState({ currentUsername: null });
+  }
+
+  render() {
+    let body;
+    if (this.state.currentUsername) {
+      body = (
+        <Profile
+          username={this.state.currentUsername}
+          onChoose={this.handleChoose}
+        />
+      );
+    } else {
+      body = <Directory onChoose={this.handleChoose} />;
+    }
+
+    return (
+      <div className="App">
+        <header>
+          <h1>PetBook</h1>
+
+          <nav>
+            {this.state.currentUsername && (
+              <button onClick={this.handleReturnToDirectoryClick}>
+                Return to directory
+              </button>
+            )}
+          </nav>
+        </header>
+
+        <main>{body}</main>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
